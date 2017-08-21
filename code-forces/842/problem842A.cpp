b6246ab7e5a2bdec38555e15706e0d70e113db4b
andrekorol <anrobits@yahoo.com.br>
2017-08-21 14:46:11 -0300

    deleted:    code-forces/842/#problem842A.cpp# 	deleted:    code-forces/842/.#problem842A.cpp 	modified:   code-forces/842/problem842A.cpp

--- code-forces/842/problem842A.cpp
+++ code-forces/842/problem842A.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+#include <typeinfo>
+
 using namespace std;
+typedef unordered_map<int,int> UM;
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello, world!" << endl;
+    int n, i, k = 0;
+    UM uMap;
+    cin >> n;
+    while (cin >> i) {
+	uMap.insert({i, k});
+	++k;
+    }
+    int rep = k - uMap.size();
+    cout << "rep = " << rep << endl;
+    cout << "k = "  << k << endl;
+        
     return 0;
 }
