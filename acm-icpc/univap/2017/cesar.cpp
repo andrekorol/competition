c6ea81463efbeccddf8762ad81f4e3788d3b49d8
andrekorol <anrobits@yahoo.com.br>
2019-04-25 14:44:48 -0300

    Add code for 2017's 'Mensagem Oculta' problem

--- acm-icpc/univap/2017/cesar.cpp
+++ acm-icpc/univap/2017/cesar.cpp
@@ -6,10 +6,20 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie();
 
-    int n, v;
+    int n, shift;
     cin >> n;
 
-
+    string s;
+    while (n--) {
+        cin >> s;
+        cin >> shift;
+        for (auto &c : s) {
+            c -= 'A';
+            c = (c - shift + 26) % 26;
+            c += 'A';
+        }
+        cout << s << '\n';
+    }
 
     return 0;
 }
--- acm-icpc/univap/2017/cesar.py
+++ acm-icpc/univap/2017/cesar.py
@@ -1,14 +1,14 @@
-from string import ascii_uppercase
-
 n = int(input())
 
 while n > 0:
     s = input()
-    v = int(input())
+    shift = int(input())
     original_str = ''
     for c in s:
-        c_index = ascii_uppercase.index(c)
-        original_index = c_index - v
-        original_str += ascii_uppercase[original_index]
+        c_ord = ord(c)
+        c_ord -= ord('A')
+        c_ord = (c_ord - shift + 26) % 26
+        c_ord += ord('A')
+        original_str += chr(c_ord)
     print(original_str)
     n -= 1
